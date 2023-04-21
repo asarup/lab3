2237ea5b7bf4c4131d4aa599d9c02ec6ad8adf5d
asarup <asarup@ucsd.edu>
2023-04-20 22:07:25 -0700

    Update ListTests.java

--- ListTests.java
+++ ListTests.java
@@ -8,14 +8,13 @@ public class ListTests {
 
     private static final String[] STR_ARR_1 = {"abc", "bc", "c"}, STR_ARR_2 = {"def", "ef", "f"};
     private List<String> stringList1, stringList2;
-    StringChecker s1, s2;
+    StringChecker s1;
 
     @Before
     public void setUp() {
         stringList1 = new ArrayList<String>(Arrays.asList(STR_ARR_1));
         stringList2 = new ArrayList<String>(Arrays.asList(STR_ARR_2));
         s1 = new StringContains("b");
-        s2 = new StringContains("e");
     }
 
     @Test
@@ -25,7 +24,7 @@ public class ListTests {
 
     @Test
     public void testMerge() {
-        assertArrayEquals(new String[]{"abc","bc","c","def","ef", "f"}, 
+        assertArrayEquals(new String[]{"abc","bc","c","def","ef","f"}, 
           ListExamples.merge(stringList1, stringList2).toArray());
     }
 
