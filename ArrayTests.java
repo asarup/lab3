cf0bcc5ef7be1c89dd609b2524256336b7da35ac
asarup <asarup@ucsd.edu>
2023-04-20 19:20:01 -0700

    Fixed ArrayExamples and wrote tests in ArrayTests

--- ArrayTests.java
+++ ArrayTests.java
@@ -2,17 +2,35 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
-	@Test 
+	
+  private int[] input1;
+  private int[] sameRef;
+  private double[] input2;
+
+  @Before
+  public void setUp() {
+    input1 = new int[]{1, 3, 5, 7, 9};
+    input2 = new double[]{3.0, 3.0, 4.0, 5.0, 6.0};
+    sameRef = input1;
+  }
+
+  
+  @Test 
 	public void testReverseInPlace() {
-    int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    assertEquals(sameRef, input1);
+    assertArrayEquals(new int[]{9, 7, 5, 3, 1}, input1);
 	}
 
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    assertNotEquals(sameRef, ArrayExamples.reversed(input1));
+    assertArrayEquals(new int[]{9, 7, 5, 3, 1}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testAverageWithoutLowest() {
+    assertEquals(4.5, ArrayExamples.averageWithoutLowest(input2), 0.001);
   }
 }
