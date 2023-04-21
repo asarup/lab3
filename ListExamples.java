3f7b628fb735b7b7958e6c34058e57cd4c4371ef
asarup <asarup@ucsd.edu>
2023-04-20 22:03:42 -0700

    Wrote ListTests and fixed ListExamples

--- ListExamples.java
+++ ListExamples.java
@@ -12,7 +12,7 @@ class ListExamples {
     List<String> result = new ArrayList<>();
     for(String s: list) {
       if(sc.checkString(s)) {
-        result.add(0, s);
+        result.add(s);
       }
     }
     return result;
@@ -40,10 +40,22 @@ class ListExamples {
     }
     while(index2 < list2.size()) {
       result.add(list2.get(index2));
-      index1 += 1;
+      index2 += 1;
     }
     return result;
   }
 
 
+}
+
+class StringContains implements StringChecker {
+  private String substring;
+  
+  public StringContains(String substring) {
+    this.substring = substring;
+  }
+
+  public boolean checkString(String s) {
+    return s.contains(substring);
+  }
 }
---
+++ ListTests.java
@@ -0,0 +1,32 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ListTests {
+
+    private static final String[] STR_ARR_1 = {"abc", "bc", "c"}, STR_ARR_2 = {"def", "ef", "f"};
+    private List<String> stringList1, stringList2;
+    StringChecker s1, s2;
+
+    @Before
+    public void setUp() {
+        stringList1 = new ArrayList<String>(Arrays.asList(STR_ARR_1));
+        stringList2 = new ArrayList<String>(Arrays.asList(STR_ARR_2));
+        s1 = new StringContains("b");
+        s2 = new StringContains("e");
+    }
+
+    @Test
+    public void testFilter() {
+        assertArrayEquals(new String[]{"abc","bc"}, ListExamples.filter(stringList1, s1).toArray());
+    }
+
+    @Test
+    public void testMerge() {
+        assertArrayEquals(new String[]{"abc","bc","c","def","ef", "f"}, 
+          ListExamples.merge(stringList1, stringList2).toArray());
+    }
+
+}
